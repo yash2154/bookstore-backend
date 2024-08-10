b3a206f746bdd47eb061e06d8f10b3e915c8fc89
yash rai <yash2154rai@gmail.com>
2024-08-10 15:54:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bookRoute = require("./routes/bookRoute.js");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.get("/", async (request, response) => {
+  return response.status(200).json({ message: "welcome to bookstore api" });
+});
+
+// book router
+app.use("/books", bookRoute);
+
+const URL = process.env.DATABASE_URL;
+const dbname = "bookstore";
+mongoose
+  .connect(`${URL}/${dbname}`)
+  .then(() => {
+    console.log("app is connected to database");
+    app.listen(5555, () => {
+      console.log("app is listening");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
