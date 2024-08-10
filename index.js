c1f6d80d6e0c196507102d3e0c7b518a6f537133
yash rai <yash2154rai@gmail.com>
2024-08-10 15:57:04 +0530

    change in env

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.ALLOW_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
